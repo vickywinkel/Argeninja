738ed68e17cbce40e4f2949e7ea40071d0f1bb72
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-10-31 15:59:16 -0300

    terminado

--- Assets/Scripts/espadaScript.cs
+++ Assets/Scripts/espadaScript.cs
@@ -16,7 +16,7 @@ public class espadaScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tiempoTotal = 1.5f;
+        tiempoTotal = 1f;
     }
 
     // Update is called once per frame
